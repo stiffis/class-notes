0f6605e5fe3062ef8705b8cf0123b09b124aead4
stiffis <steve.ildefonso@utec.edu.pe>
2025-01-08 17:28:22 -0500

    vault backup: 08/01/2025 17:28:22

--- Ciclo 3/Programación III/Laboratorio/S2A/E2/E2.cpp
+++ Ciclo 3/Programación III/Laboratorio/S2A/E2/E2.cpp
@@ -4,6 +4,10 @@ class Complejo {
 public:
 	Complejo(double a, double b);
 	~Complejo();
+friend std::ostream &operator<<(std::ostream &os, const  Complejo& item){
+	os << "q: " << item.real << " + " << item.img<<"i" << std::endl;
+	return os;
+}
 Complejo operator+(Complejo& otro){
 	Complejo result(this->real+otro.real,this->img+otro.img);
 	return result;
@@ -18,14 +22,20 @@ Complejo operator*(Complejo& otro){
 	Complejo result(c,d);
 	return result;
 }
+Complejo& operator++() {
+	this->img+=1;
+	this->real+=1;
+	return *this;
+}
+
+//Complejo operator++(){
+//	Complejo temp = *this;
+
+//}
 bool operator==(Complejo& otro){
 	return (this->real==otro.real)&&(this->img==otro.img);
 }
 
-friend std::ostream &operator<<(std::ostream &os, const  Complejo& item){
-	os << "q: " << item.real << " + " << item.img<<"i" << std::endl;
-	return os;
-}
 // TODO: TERMINAR EL >>
 friend std::istream &operator>>(std::istream &is,  Complejo& item){
 	double x,y;
@@ -55,5 +65,6 @@ int main () {
 	
 	std::cout << c1 << std::endl;
 	std::cout << c2 << std::endl;
+	std::cout << c1*c2 << std::endl;
 	return 0;
 }
