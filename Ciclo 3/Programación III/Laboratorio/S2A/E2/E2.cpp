ff8e75bbde4f194c6c2ab9d3b90da3847e9f5cf6
stiffis <steve.ildefonso@utec.edu.pe>
2025-01-08 17:06:00 -0500

    vault backup: 08/01/2025 17:06:00

--- Ciclo 3/Programación III/Laboratorio/S2A/E2/E2.cpp
+++ Ciclo 3/Programación III/Laboratorio/S2A/E2/E2.cpp
@@ -1,8 +1,25 @@
+#include <iostream>
 class Complejo {
 public:
 	Complejo(double a, double b);
 	~Complejo();
-
+//bool operator==(const Coor &item1, const Coor &item2){
+//	return (item1.x == item2.x) && (item 1.y == item2.y);
+//}
+Complejo operator+(Complejo& otro){
+	Complejo result(this->real+otro.real,this->img+otro.img);
+	return result;
+}
+Complejo operator-(Complejo& otro){
+	Complejo result(this->real-otro.real,this->img-otro.img);
+	return result;
+}
+Complejo operator*(Complejo& otro){
+	double c = (this->real*otro.real) - (this->img*otro.img);
+	double d = (this->img*otro.real) + (this->real*otro.img);
+	Complejo result(c,d);
+	return result;
+}
 private:
 	double real;
 	double img;
@@ -13,4 +30,11 @@ Complejo::Complejo(double a, double b) : real(a), img(b){
 }
 
 Complejo::~Complejo() {
+}
+
+
+int main () {
+	
+
+	return 0;
 }
