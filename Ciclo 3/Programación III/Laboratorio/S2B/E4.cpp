e823477118752e8dd1a4ba6127a84eb0b93c7fe3
stiffis <steve.ildefonso@utec.edu.pe>
2025-01-09 07:52:10 -0500

    vault backup: 09/01/2025 07:52:10

--- Ciclo 3/Programación III/Laboratorio/S2B/E4.cpp
+++ Ciclo 3/Programación III/Laboratorio/S2B/E4.cpp
@@ -6,7 +6,9 @@ public:
 	Numero &operator=(Numero &&) = default;
 	Numero &operator=(const Numero &) = default;
 	~Numero();
+	virtual int termino(int n){
 
+	}
 private:
 	
 };
@@ -15,4 +17,69 @@ Numero::Numero() {
 }
 
 Numero::~Numero() {
+}
+//Derivadas---------------------------------------
+class Primos : public Numero{
+public:
+	Primos();
+	Primos(Primos &&) = default;
+	Primos(const Primos &) = default;
+	Primos &operator=(Primos &&) = default;
+	Primos &operator=(const Primos &) = default;
+	~Primos();
+	int termino(int n)override{
+		
+	}
+private:
+	
+};
+
+Primos::Primos() {
+}
+
+Primos::~Primos() {
+}
+//---
+class CuadradoPerfecto : public Numero{
+public:
+	CuadradoPerfecto();
+	CuadradoPerfecto(CuadradoPerfecto &&) = default;
+	CuadradoPerfecto(const CuadradoPerfecto &) = default;
+	CuadradoPerfecto &operator=(CuadradoPerfecto &&) = default;
+	CuadradoPerfecto &operator=(const CuadradoPerfecto &) = default;
+	~CuadradoPerfecto();
+
+	int termino(int n)override{
+		
+	}
+private:
+	
+};
+
+CuadradoPerfecto::CuadradoPerfecto() {
+}
+
+CuadradoPerfecto::~CuadradoPerfecto() {
+}
+//---
+class Fibonacci : public Numero{
+public:
+	Fibonacci();
+	Fibonacci(Fibonacci &&) = default;
+	Fibonacci(const Fibonacci &) = default;
+	Fibonacci &operator=(Fibonacci &&) = default;
+	Fibonacci &operator=(const Fibonacci &) = default;
+	~Fibonacci();
+
+	int termino(int n)override{
+
+	}
+private:
+	
+};
+
+Fibonacci::Fibonacci() {
+}
+
+Fibonacci::~Fibonacci() {
 }
